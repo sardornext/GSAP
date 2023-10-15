dc4140269e5dbb9b3adf91cfc91660ea9fed4947
Sardor <139249785+sardornext@users.noreply.github.com>
2023-10-15 19:05:04 +0500

    Animated web page 2

--- script.js
+++ script.js
@@ -18,3 +18,5 @@ const obj = {x: 0, y: 0}
 gsap.to(obj, {x: 100, onUpdate: () => {
     console.log(obj.x, obj.y)
 }});
+
+gsap.to('.box', {duration: 2, x: 100, rotation: 360, ease: 'bounce.out', stagger: 0.5, repeat: -1, yoyo: true})
